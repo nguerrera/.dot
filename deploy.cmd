22882b2915ef4e49a07d2d5884c202772302edc6
Nick Guerrera <nick@technogenic.net>
2016-04-05 11:00:19 -0700

    Make HOME environment var permanent on Windows

--- deploy.cmd
+++ deploy.cmd
@@ -7,11 +7,12 @@ net file >nul 2> nul && goto :Elevated
 echo error: Administrator rights are required to make symbolic links on Windows.
 exit /b 1
 
-
 :Elevated
 if not defined HOME (
     set HOME=%USERPROFILE%
 )
+
+setx HOME %HOME%
 
 for %%f in (%~dp0.\.*) do (
     if exist %HOME%\%%~nxf (
