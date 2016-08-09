7bb76f8fce8523373fb387a358a681b64177d963
Nick Guerrera <nick@technogenic.net>
2016-08-09 10:09:44 -0700

    Use hard links

--- deploy.cmd
+++ deploy.cmd
@@ -1,16 +1,7 @@
-:: Symbolically link to all of the dot files in this directory from the
+:: hard link to all of the dot files in this directory from the
 :: home directory.
 
 @echo off
-
-net file >nul 2> nul && goto :Elevated
-echo error: Administrator rights are required to make symbolic links on Windows.
-exit /b 1
-
-:Elevated
-if not defined HOME (
-    set HOME=%USERPROFILE%
-)
 
 setx HOME %HOME%
 
@@ -18,5 +9,5 @@ for %%f in (%~dp0.\.*) do (
     if exist %HOME%\%%~nxf (
         del /P %HOME%\%%~nxf
     )
-    mklink %HOME%\%%~nxf %%f
+    mklink /h %HOME%\%%~nxf %%f
 )
