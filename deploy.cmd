d5082d347c55a75fd6cb75dae0ed7bac4d540c30
Nick Guerrera <nick@technogenic.net>
2020-05-22 09:59:49 -0700

    Make deploy smarter

--- deploy.cmd
+++ deploy.cmd
@@ -1,21 +1,35 @@
-:: hard link to all of the dot files in this directory from the
-:: home directory.
-
 @echo off
 
-if not defined HOME (
-    set HOME=%USERPROFILE%
-)
+:: set HOME=%USERPROFILE% permanently
+:: emacs uses %APPDATA% otherwise
+set HOME=%USERPROFILE%
+setx HOME %HOME% >nul
 
-setx HOME %HOME%
+setlocal enabledelayedexpansion
 
-for %%f in (%~dp0.\.*) do (
-    if  "%%~nxf" neq ".gitconfig" (
-        if exist %HOME%\%%~nxf (
-            del /P %HOME%\%%~nxf
+:: chain .gitconfig, allowing machine specific settings to be maintained using
+:: `git config --global` while keeping only the settings that apply to all
+:: machines in this repo
+git config --global include.path %~dp0.gitconfig
+
+:: make symbolic links to files
+call :make_links
+
+:: make junctions to directories
+call :make_links /d /j
+
+goto :eof
+
+:make_links
+for %1 %%f in (%~dp0.\.*) do (
+    set filename=%%~nxf
+    if "!filename:~0,4!" neq ".git" (
+        if exist %HOME%\!filename! (
+            echo warning: '%HOME%\!filename!' already exists, not overwriting
+        ) else (
+            mklink %2 %HOME%\!filename! %%f
         )
-        mklink %HOME%\%%~nxf %%f
     )
 )
+goto :eof
 
-git config --global include.path %~dp0.gitconfig
