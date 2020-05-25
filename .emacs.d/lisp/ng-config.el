7fd138311093c73856565fa0465545ff94a08af0
Nick Guerrera <nick@technogenic.net>
2020-05-24 17:11:07 -0700

    Major rewrite of emacs config
    Startup time and package management is much improved

---
+++ .emacs.d/lisp/ng-config.el
@@ -0,0 +1,275 @@
+;; Prevent Emacs from making #auto-save#, .#lock and backup~ files all
+;; over the bloody place. We do this first to avoid making a mess when
+;; hacking on a broken init.
+;;
+;; https://www.emacswiki.org/emacs/BackupDirectory
+;; https://www.emacswiki.org/emacs/LockFiles
+
+;; auto-save and backups are nice, but keep them out of sight
+(setq
+ auto-save-file-name-transforms '((".*" "~/.emacs.d/saves/" t))
+ backup-directory-alist         '(("." . "~/.emacs.d/saves/"))
+ backup-by-copying              t
+ delete-old-versions            t
+ kept-new-versions              5
+ kept-old-versions              0
+ version-control                t
+ )
+
+;; but lockfiles are terrible and can't be moved, so disable them
+(setq create-lockfiles nil)
+
+(setq package-archives
+      '(("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("elpa"         . "https://elpa.gnu.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("elpa"         . 5)
+        ("melpa"        . 0)))
+
+;; disable startup message
+(setq inhibit-startup-message t)
+
+;; leave the scratch buffer empty by default
+(setq initial-scratch-message nil)
+
+;; follow symlinks to source-controlled locations without prompting
+(setq vc-follow-symlinks t)
+
+;; save automatic customizations in their own file
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; never use tabs to indent
+(setq-default indent-tabs-mode nil)
+
+;; truncate long lines, don't wrap
+(setq-default truncate-lines t)
+
+;; triple productivity
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; don't beep
+(setq
+ visible-bell t
+ ring-bell-function 'ignore)
+
+;; highlight matching parens
+(show-paren-mode 1)
+
+;; hide menu bar and tool bar
+(progn
+  (menu-bar-mode -1)
+  (tool-bar-mode -1))
+
+;; show line and column numbers
+(progn
+  (line-number-mode 1)
+  (column-number-mode 1))
+
+;; don't blink the cursor
+(blink-cursor-mode -1)
+
+;; refresh unsaved files
+(global-auto-revert-mode 1)
+
+;; http://www.emacswiki.org/SmoothScrolling
+(setq
+ mouse-wheel-scroll-amount        '(1 ((shift) . 1)) 
+ mouse-wheel-progressive-speed    nil
+ mouse-wheel-follow-mouse         t            
+ scroll-step                      1
+ scroll-preserve-screen-position  1
+ hscroll-margin                   0
+ hscroll-step                     1
+ )
+
+;; use ls-lisp everywhere to portably group directories first and get
+;; away from "ls does not support --dired" warnings
+(setq ls-lisp-use-insert-directory-program nil)
+(setq ls-lisp-dirs-first t)
+(require 'ls-lisp)
+
+;; start server for emacsclient
+;; https://www.emacswiki.org/emacs/EmacsClient
+(when window-system
+  (server-start)
+  ;; don't nag about live clients when killing buffers or exiting emacs
+  (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+  (remove-hook 'kill-emacs-query-functions 'server-kill-emacs-query-function))
+
+;; set font
+(when window-system
+  (cl-dolist (font '("Consolas-11" "DejaVu Sans Mono-11.5"))
+    (when (find-font (font-spec :name font))
+      (add-to-list 'default-frame-alist `(font . ,font))
+      (set-frame-font font) ;; not strictly necessary, but makes UI
+                            ;; look nicer sooner
+      (cl-return))))
+
+(require 'use-package)
+(require 'ng-lib)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Key bindings
+
+;; http://www.emacswiki.org/CuaMode
+(cua-mode 1)
+(setq cua-auto-tabify-rectangles nil)
+(setq cua-keep-region-after-copy t)
+
+(ng/set-keys
+ "C-k"         ng/C-k
+ "C-w"         ng/C-w
+ "C-a"         ng/home
+ "M-m"         ng/home
+ "<home>"      ng/home
+ "C-e"         ng/end
+ "<end>"       ng/end
+ "C-c C-c"     ng/done
+ "C-x C-k"     ng/kill-other-buffer-and-window
+ "C-x k"       ng/kill-this-buffer-and-window
+ "<M-S-up>"    ng/rectangle-mark-up
+ "<M-S-down>"  ng/rectangle-mark-down
+ "<M-S-left>"  ng/rectangle-mark-left
+ "<M-S-right>" ng/rectangle-mark-right
+ "M-n"         cua-scroll-up
+ "M-p"         cua-scroll-down
+ "C-m"         newline-and-indent
+ "C-x C-b"     ibuffer
+ "<C-tab>"     next-buffer
+ "<C-S-tab>"   previous-buffer
+ "C-;"         comment-line
+ "C-x SPC"     cua-rectangle-mark-mode
+ )
+
+;; remove minor mode conflicts with C-c C-c
+(progn
+  (add-hook 'bat-mode-hook (lambda () (define-key bat-mode-map (kbd "C-c C-c") nil)))
+  (add-hook 'conf-mode-hook (lambda () (define-key conf-mode-map (kbd "C-c C-c") nil))))
+
+;; disable archaic "secondary selection" on alt clicks, which we'll
+;; replace with modern multiple cursor/rectangle functionality
+(progn
+  (global-unset-key (kbd "M-<drag-mouse-1>"))
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  (global-unset-key (kbd "M-<mouse-1>"))
+  (global-unset-key (kbd "M-<mouse-2>"))
+  (global-unset-key (kbd "M-<mouse-3>")))
+
+(use-package esup
+  :commands esup)
+
+(use-package move-text
+  :bind
+  ("<M-up>"   . move-text-up)
+  ("<M-down>" . move-text-down))
+
+;; https://www.emacswiki.org/emacs/GotoAddress
+(use-package goto-addr
+  :hook ((find-file . goto-address-mode)))
+
+;; http://emacswiki.org/InteractivelyDoThings
+(use-package ido
+  :init
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (setq ido-enable-flex-matching t))
+
+;; https://github.com/DarwinAwardWinner/ido-completing-read-plus
+(use-package ido-completing-read+
+  :requires ido
+  :config
+  (ido-ubiquitous-mode 1))
+
+;; https://github.com/lewang/flx
+(use-package flx-ido
+  :requires ido
+  :init
+  (setq ido-use-faces nil)
+  :config
+  (flx-ido-mode 1))
+
+;; https://github.com/larkery/ido-grid-mode.el
+(use-package ido-grid-mode
+  :requires ido
+  :init
+  (setq
+   ido-grid-mode-max-columns  1
+   ido-grid-mode-max-rows     8
+   ido-grid-mode-min-rows     8
+   ido-grid-mode-scroll-down  #'ido-grid-mode-next-row
+   ido-grid-mode-scroll-up    #'ido-grid-mode-previous-row
+   ido-grid-mode-scroll-wrap  nil
+   ido-grid-mode-order        nil)
+  :config
+  (ido-grid-mode 1))
+
+;; https://github.com/bbatsov/projectile
+(use-package projectile
+  :bind
+  ("C-," . projectile-find-file)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :config
+  (projectile-mode 1))
+
+;; https://magit.vc/
+(use-package magit
+  :bind
+  ("C-x g" . magit-status)
+  :init
+  ;; open magit status in same window
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
+
+;; https://www.emacswiki.org/emacs/EdiffMode
+(use-package ediff
+  :after magit
+  :init
+  (setq
+   ;; don't open ediff control in new frame
+   ediff-window-setup-function 'ediff-setup-windows-plain
+   ;; use side-by-side ediff by default
+   ediff-split-window-function 'split-window-horizontally))
+
+(use-package multiple-cursors
+  :commands
+  mc/edit-lines
+  :bind
+  ("M-<mouse-1>" . mc/add-cursor-on-click))
+
+(use-package expand-region
+  :bind
+  ("<C-S-right>" . er/expand-region)
+  ("<C-S-left>"  . er/contract-region))
+  
+(use-package ace-jump-mode
+  :bind
+  ("C-j" . ace-jump-mode))
+
+(use-package smex
+  :bind
+  ("M-x" . smex))
+
+(use-package csharp-mode
+  :mode "\\.cs\\'")
+
+;; C/C++/C#/Java and other c-mode based language
+(progn
+  (add-hook
+   'c-mode-common-hook
+   (lambda ()
+     (c-set-style "bsd")
+     (c-set-offset 'case-label '+)
+     (setq c-basic-offset 4))))
+
+;; Perl 
+;; http://www.emacswiki.org/emacs/CPerlMode
+(progn
+  (defalias 'perl-mode 'cperl-mode)
+  (add-hook
+   'cperl-mode-hook
+   (lambda ()
+     (setq cperl-indent-level 4)
+     (setq cperl-brace-offset (- cperl-indent-level))
+     (setq cperl-invalid-face nil))))
---
+++ .emacs.d/lisp/ng-lib.el
@@ -0,0 +1,118 @@
+(defmacro ng/dopairs (names pairs body)
+  "Process list in pairs."
+  (declare (indent 2))
+  `(let ((__p ,pairs))
+     (while __p
+       (let ((,(car  names) (car  __p))
+             (,(cadr names) (cadr __p)))
+         ,body
+         (setq __p (cddr __p))))))
+
+;; helper to global-set-key in pairs with fewer parens
+(defmacro ng/set-keys (&rest pairs)
+  (ng/dopairs (key function) pairs
+    (global-set-key (eval `(kbd ,key)) function)))
+
+(defun ng/home (&optional arg)
+  "Move back to indentation. If already there, move to beginning
+of line. If a region is selected, create multiple cursors in the
+selected lines before moving."
+  (interactive "p")
+  (setq arg (or arg 1))
+  (if (/= arg 1)
+      (move-beginning-of-line arg)
+    (progn
+      (if (and (use-region-p) (fboundp 'mc/edit-lines))
+          (mc/edit-lines))
+      (if (ng/at-indentation-p)
+          (move-beginning-of-line 1)
+        (back-to-indentation)))))
+
+(defun ng/end (&optional arg)
+  "Move back to indentation. If already there, move to beginning
+of line. If a region is selected, create multiple cursors in the
+selected lines before moving."
+  (interactive "p")
+  (setq arg (or arg 1))
+  (if (/= arg 1)
+      (move-end-of-line arg)
+    (if (and (use-region-p) (fboundp 'mc/edit-lines))
+        (mc/edit-lines))
+    (move-end-of-line 1)))
+
+(defun ng/at-indentation-p ()
+  "Check if the cursor is already back to indentation."
+  (interactive)
+  (let ((old-column (current-column)))    
+    (save-excursion
+      (back-to-indentation)
+      (= old-column (current-column)))))
+
+(defun ng/rectangle-mark-down (&optional arg)
+  (interactive "p")          
+  (unless cua--rectangle (cua-set-rectangle-mark))
+  (cua-resize-rectangle-down arg))
+                             
+(defun ng/rectangle-mark-up (&optional arg)
+  (interactive "p")          
+  (unless cua--rectangle (cua-set-rectangle-mark))
+  (cua-resize-rectangle-up arg))
+
+(defun ng/rectangle-mark-left (&optional arg)
+  (interactive "p")
+  (unless cua--rectangle (cua-set-rectangle-mark))
+  (cua-resize-rectangle-left arg))
+
+(defun ng/rectangle-mark-right (&optional arg)
+  (interactive "p")
+  (unless cua--rectangle (cua-set-rectangle-mark))
+  (cua-resize-rectangle-right arg))
+
+(defun ng/C-w (&optional arg)
+  "If there is an active region, kill it. Otherwise, kill the
+preceding word. Allows C-w to backward-kill-word like it does on
+the terminal, but still kill-region when something is selected."
+  (interactive "p")
+  (if (use-region-p)
+      (kill-region (region-beginning) (region-end))
+    (backward-kill-word arg)))
+
+(defun ng/C-k (&optional arg)
+  "If there is an active region, kill it. Otherwise, kill the
+following line. Adds symmetry to the mnemonic C-k=kill:C-y==yank."
+  (interactive "P")
+  (if (use-region-p)
+      (kill-region (region-beginning) (region-end))
+    (kill-line arg)))
+
+(defun ng/kill-this-buffer-and-window ()
+  "Kill the current buffer and delete its window."
+  (interactive)
+  (let ((window (selected-window))
+                (buffer (current-buffer)))
+    (if (window-parent window)
+        (delete-window window))
+    (kill-buffer buffer)))
+
+(defun ng/kill-other-buffer-and-window ()
+  "Kill the buffer in the other window and delete its window."
+  (interactive)
+  (let ((old-window (selected-window))
+        (old-buffer (current-buffer)))
+    (other-window 1)
+    (let ((new-window (selected-window))
+          (new-buffer (current-buffer)))
+      (unless (eq new-buffer old-buffer)
+        (kill-buffer new-buffer))
+      (unless (eq new-window old-window)
+        (delete-window new-window)))))
+
+(defun ng/done ()
+  "Save the current buffer, kill it, and delete its window."
+  (interactive)
+  (save-buffer)
+  (if server-buffer-clients
+      (server-done)
+    (ng/kill-this-buffer-and-window)))
+
+(provide 'ng-lib)
