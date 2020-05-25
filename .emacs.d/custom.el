7ed94c46c647c9185a5b1a56480d511dc494aa8a
Nick Guerrera <nick@technogenic.net>
2020-05-25 08:01:31 -0700

    Fix esup profiling and handling of custom-file
    esup was barfing on loading a file without .el extension
    
    custom-file wasn't loaded or tracked by git

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (use-package smex projectile multiple-cursors move-text magit ido-grid-mode ido-completing-read+ flx-ido expand-region esup csharp-mode ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/lisp/ng-config.el
+++ .emacs.d/lisp/ng-config.el
@@ -7,9 +7,6 @@
 ;; follow symlinks to source-controlled locations without prompting
 (setq vc-follow-symlinks t)
 
-;; save automatic customizations in their own file
-(setq custom-file "~/.emacs.d/custom.el")
-
 ;; never use tabs to indent
 (setq-default indent-tabs-mode nil)
 
