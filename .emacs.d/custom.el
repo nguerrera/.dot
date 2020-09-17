773dbd27ae84b4d5747d63a5bb6c0320b1a7c4e2
Nick Guerrera <nick@technogenic.net>
2020-09-16 19:06:47 -0700

    Fix highlighting issue

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yaml-mode use-package typescript-mode smex projectile powershell multiple-cursors move-text markdown-mode magit ido-grid-mode ido-completing-read+ flx-ido expand-region esup csharp-mode ace-jump-mode)))
+   (quote
+    (yaml-mode use-package typescript-mode smex projectile powershell multiple-cursors move-text markdown-mode magit ido-grid-mode ido-completing-read+ flx-ido expand-region esup csharp-mode ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
