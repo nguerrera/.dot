497067bccef675002e385854047815df514c597f
Nick Guerrera <nick@technogenic.net>
2025-02-23 10:55:28 -0600

    Alphabetize winget list

--- bin/install-winget-tools.cmd
+++ bin/install-winget-tools.cmd
@@ -6,15 +6,16 @@ winget install -e --id Git.Git
 winget install -e --id GitHub.cli
 winget install -e --id KirillOsenkov.MSBuildStructuredLogViewer
 winget install -e --id Microsoft.AzureCLI
+winget install -e --id Microsoft.DotNet.SDK.8
+winget install -e --id Microsoft.DotNet.SDK.9
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.WinDbg
 winget install -e --id Microsoft.Sysinternals
 winget install -e --id OpenJS.NodeJS.LTS
 winget install -e --id ScooterSoftware.BeyondCompare.5
 winget install -e --id TortoiseGit.TortoiseGit
-winget install -e --id Microsoft.PowerShell
-winget install -e --id Microsoft.DotNet.SDK.8
-winget install -e --id Microsoft.DotNet.SDK.9
-winget install -e --id Microsoft.VisualStudioCode
+
 winget install -e --name "ILSpy Fresh" --source msstore --accept-package-agreements
 
 :: Emacs installer doesn't register itself so running this script twice will
